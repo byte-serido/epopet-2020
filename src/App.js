16042cac8bc4edf51ec090598fa25b7e7b5782b0
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-09-20 12:19:06 -0300

    Adicionando components e editorconfig Adicionando About; Adicionando editorconfig;

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './App.css'
+import About from './pages/About';
 import Footer from './pages/Footer';
 
 function App() {
-  return (
-    <>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <About />
+            <Footer />
+        </>
+    );
 }
 
 export default App;
