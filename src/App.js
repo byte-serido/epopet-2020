cfba632a2643c410fbfc8d25376cef8558eacc28
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-09-20 14:29:09 -0300

    Adicionando o Header no App;

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css'
+import Header from './pages/Header';
 import About from './pages/About';
 import Footer from './pages/Footer';
 
 function App() {
     return (
         <>
+            <Header />
             <About />
             <Footer />
         </>
