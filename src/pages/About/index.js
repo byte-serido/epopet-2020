71cf3c5764bb784b915d250d86a0ed36d5090444
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-09-27 21:48:48 -0300

    Mudando estilizações; Alterando o nome da pasta Local; Adicionando font roboto; Removendo warnings dos vídeos do youtube; Aletrando estilizações;

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -6,16 +6,18 @@ function About() {
         <div className="about-root">
             <div className="style-fonts">
                 <h1>Sobre o EPOPET</h1>
-                <p>O Encontro Potiguar dos Grupos PETs (EPOPET) é um evento que
-                    objetiva aproximar os alunos que participam do Programa de
-                    Educação Tutorial (PET), desenvolvido por instituições de ensino
-                    superior do estado do Rio Grande do Norte. O evento proporciona
-                    aos participantes a troca de experiências, a realização de atividades
-                    que aprimoram a formação acadêmica dos discentes e a efetivação
-                    das diretrizes nacionais do programa. Em 2020 acontecerá a sua
-                    vigésima edição, entre 07 e 08 - 14 e 22 de novembro, esse ano de forma
-                    totalmente online. Está à frente da organização do evento o
-                    PET Comunidade Urbana Seridó (PETCoUrSe).</p>
+                <div className="text">
+                    <p>O Encontro Potiguar dos Grupos PETs (EPOPET) é um evento que
+                        objetiva aproximar os alunos que participam do Programa de
+                        Educação Tutorial (PET), desenvolvido por instituições de ensino
+                        superior do estado do Rio Grande do Norte. O evento proporciona
+                        aos participantes a troca de experiências, a realização de atividades
+                        que aprimoram a formação acadêmica dos discentes e a efetivação
+                        das diretrizes nacionais do programa. Em 2020 acontecerá a sua
+                        vigésima edição, entre 07 e 08 - 14 e 22 de novembro, esse ano de forma
+                        totalmente online. Está à frente da organização do evento o
+                        PET Comunidade Urbana Seridó (PETCoUrSe).</p>
+                </div>
             </div>
         </div>
     )
